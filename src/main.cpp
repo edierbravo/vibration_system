6699ce6a38a235c67f06764db9b40f8eecb5a786
edierbra <edierbra@unicauca.edu.co>
2023-01-23 18:01:54 -0500

    Conectado v1 Buzer RFID Vibracion

--- src/main.cpp
+++ src/main.cpp
@@ -1,212 +1,221 @@
-//Este skecht se ha desarrollado para activar el bombillo a partir del umbral
-//también el ventilador cuando la temperatura suba por encima de cierto umbral
+
+///////////////////// vibrador /////////////////
+int vb=34; // pin digital
+int Avb=4; // pin analogico
+int Dig_out = 0; //variable
+int Ana_out = 0; // variable
+
+int B = 1;
+///////////////////// RFID //////////////////
+// LISTA DE REPRODUCCION - ESP32 - ESP8266
+// https://www.youtube.com/playlist?list=PLZHVfZzF2DYID9jGK8EpcMni-U2CSTrw3
+
+// LISTA DE REPRODCUION DEL CURSO DE ARDUINO DESDE CERO
+// https://www.youtube.com/playlist?list=PLZHVfZzF2DYJeLXXxz6YtpBj4u7FoGPWN
+/* ESP 32 NODE MCU
+  Vcc <-> 3V3 (o Vin(5V) según la versión del módulo)
+  RST (Reset) <-> D22
+  GND (Masse) <-> GND
+  MISO (Master Input Slave Output) <-> 19
+  MOSI (Master Output Slave Input) <-> 23
+  SCK (Serial Clock) <-> 18
+  SS/SDA (Slave select) <-> 5
+*/
+/* ESP 8266 NODE MCU
+  Vcc <-> 3V3 (o Vin(5V) según la versión del módulo)
+  RST (Reset) <-> D0
+  GND (Tierra) <-> GND
+  MISO (Master Input Slave Output) <-> D6
+  MOSI (Master Output Slave Input) <-> D7
+  SCK (Serial Clock) <-> D5
+  SS/SDA (Slave select) <-> D8
+*/
+
 #include <Arduino.h>
-#include <ChainableLED.h>
-#include <Wifi.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
-//*************** Coneción a ThinkSpeak *********
-#include <ThingSpeak.h>
+#if defined(ESP32)
+  #define SS_PIN 5
+  #define RST_PIN 22
+#elif defined(ESP8266)
+  #define SS_PIN D8
+  #define RST_PIN D0
+#endif
 
-//------------------------- Activar WIFI ESP8266 -----------------------
-// #include <ESP8266WiFi.h>
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key;
+// Init array that will store new NUID
+byte nuidPICC[4];
 
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+String DatoHex;
+/*const String UserReg_1 = "F9049E2E";
+const String UserReg_2 = "B33786A3";
+const String UserReg_3 = "7762C83B";*/
 
-// Información del Canal y Campos de ThingSpeak
-char thingSpeakAddress[] = "api.thingspeak.com";
-unsigned long channelID = 2014575;
-char* readAPIKey = (char*)"MCVF18UY83B6ODE9"; //"70GGTLNT0EMFP0WO";
-char* writeAPIKey = (char*)"CE9J7FJ9UB7CANQH"; //"7ZBZ9LU15LQRYKRF";
-const unsigned long postingInterval = 20L * 1000L;
-unsigned int dataFieldOne = 1;                       // Calpo para escribir el estado de la Temperatura
-unsigned int dataFieldTwo = 2;                       // Campo para escribir el estado del Bombillo
-unsigned int dataFieldThree = 3;                     // Campo para escribir el estado del ventilador
-unsigned int dataFieldFour = 4;                      // FCampo para enviar el tiempo de medición
-//*************** Fin Conección ThinkSpeak *******
-
-
-char ssid[] = "Bravo";//"TP-Link_B520";
-char password[] = "12345678"; //"67097135";
-WiFiClient client;              //Cliente Wifi para ThingSpeak
-//-------------------------- Fin Configuración WIFI ESP8266 --------------
-
-//const int sensorluzpin = A3;    //Fotocelda Grove
-const int bombillopin = 2;      //Simulado con un led 13 en Arduino
-const int ventiladorpin = 5;    //Relay del ventilador
-const int temperaturapin = 36;  //Temperatura Grove 
-
-//Variables Globales
-int umbralLuz = 500;            //Es el umbral en el cual se enciende el bombillo
-int umbralTemperatura = 27;     //27 / Es el umbral en el cual se enciende el ventilador
-float luminosidad;              //Toma el valor en voltaje
-float temperatura;              //Toma el valor en grados
-boolean estadoventilador=false; //false = apagado
-boolean estadobombillo = false; //false = apagado
-
-//Métodos para encapsular las funcionalidades
-//Simular lectura de fotocelda 
-long fotoceldafuncion()
-{
-  return random(1023);
+String UserReg[] = {"F9049E2E", "B33786A3", "7762C83B"};
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+String printHex(byte *buffer, byte bufferSize)
+{  
+   String DatoHexAux = "";
+   for (byte i = 0; i < bufferSize; i++) 
+   {
+       if (buffer[i] < 0x10)
+       {
+        DatoHexAux = DatoHexAux + "0";
+        DatoHexAux = DatoHexAux + String(buffer[i], HEX);  
+       }
+       else { DatoHexAux = DatoHexAux + String(buffer[i], HEX); }
+   }
+   
+   for (int i = 0; i < DatoHexAux.length(); i++) {DatoHexAux[i] = toupper(DatoHexAux[i]);}
+   return DatoHexAux;
 }
 
-//Funcion para obtener los valores de los sensores
-void LeerSensores(void)
-{
-   //leer el sensor de luz
-   //luminosidad = analogRead(sensorluzpin); 
-   luminosidad = fotoceldafuncion();
-
-    //recibe la temperatura para el sensor LM35
-   //temperatura = analogRead(temperaturapin);   
-   //temperatura = (5.0 * temperatura * 100.0)/1024.0; 
- 
-   //Lee estado de sensor de Temperatura para GROVE temp
-   int B=3975; //Valor del termistor
-   temperatura = analogRead(temperaturapin); //Obtencion del valor leido
-   float resistance=(float)(1023-temperatura)*10000/temperatura; //Obtencion del valor de la resistencia
-   temperatura=1/(log(resistance/10000)/B+1/298.15)-273.15; //Calculo de la temperatura
-}
-
-void ImprimirValoresSensores(void)
-{
- //Imprimir los valores sensados
-  Serial.println("========================================");
-  
- //Temeratura
- Serial.print("Temperatura: ");
- Serial.print(temperatura);
- Serial.print(" ");
- Serial.println(" ℃ ");
-
- //Luminosidad
- Serial.print("Luminosidad: ");
- Serial.print(luminosidad);
- Serial.print(" ");
- Serial.println(" V. ");
-
- //Estado del Bombillo
- if (estadobombillo == false)
-    Serial.println("Bombillo Apagado");
- else
-    Serial.println("Bombillo Encendido");
-
- //Estado del Ventilador
- if (estadoventilador == false)
-    Serial.println("Ventilador Apagado");
- else
-    Serial.println("Ventilador Encendido");
-}
-
-boolean UmbraldeTemperatura(float umbral)
-{
-  if(temperatura > umbral){
-    digitalWrite(ventiladorpin, HIGH); 
-    delay(1000);
-    return true;
-  }  
-  else{
-    digitalWrite(ventiladorpin, LOW);  
-    delay(10); 
-    return false;
-  } 
-}
-
-boolean UmbraldeLuz(float umbral)
-{
-  //Envia una señal que activa o desactiva el relay
-  if(luminosidad < umbral){
-    digitalWrite(bombillopin, HIGH);
-    delay(1000);
-    return true;
-  }   
-  else{
-    digitalWrite(bombillopin, LOW);
-    delay(10);
-    return false;  
-  }
-}
-
-// Use this function if you want to write a single field
-int writeTSData( long TSChannel, unsigned int TSField, float data ){
-  int  writeSuccess = ThingSpeak.writeField( TSChannel, TSField, data, writeAPIKey ); // Write the data to the channel
-  if ( writeSuccess ){
-    //lcd.setCursor(0, 1);
-    //lcd.print("Send ThinkSpeak");
-    Serial.println( String(data) + " written to Thingspeak." );
+int verifivarRfid( String UserRegisters[], String Dato){
+  int a = 2;
+  for (int i = 0; i < sizeof(UserRegisters) ; i++)
+  {
+    Serial.println(sizeof(UserRegisters));
+    Serial.println(UserReg[i]);
+    if(UserReg[i] == DatoHex)
+    {
+      String S1 = "USUARIO "+String(i)+" - PUEDE INGRESAR"; 
+      Serial.println(S1);
+      return a = 1;    
+    }else{
+      String S2 = "NO ESTA REGISTRADO - PROHIBIDO EL INGRESO"; 
+      Serial.println(S2);
+      return a = 0;  
     }
-    
-    return writeSuccess;
+  }
+  return a;
 }
 
-//use this function if you want multiple fields simultaneously
-int write2TSData( long TSChannel, unsigned int TSField1, 
-                  float field1Data,unsigned int TSField2, long field2Data,
-                  unsigned int TSField3, long field3Data ,
-                  unsigned int TSField4, long field4Data ){
-
-  ThingSpeak.setField( TSField1, field1Data );
-  ThingSpeak.setField( TSField2, field2Data );
-  ThingSpeak.setField( TSField3, field3Data );
-  ThingSpeak.setField( TSField4, field4Data );
-
-  int printSuccess = ThingSpeak.writeFields( TSChannel, writeAPIKey );
-  return printSuccess;
+int LeerVibracion(int vb, int Avb){
+  Dig_out = digitalRead(vb);
+  Ana_out = analogRead(Avb);
+  Serial.print("Anaolog : ");
+  int AnaV = Ana_out;//(Ana_out- 4095)*-1;
+  Serial.println(AnaV);
+  int AnaV1 = (Ana_out- 4095)*-1;
+  Serial.println(AnaV1);
+  Serial.print("          Digital :");
+  Serial.println(Dig_out);
+  return AnaV1;
 }
 
-//metodo cliente para controlar los eventos R1 y R2
-void setup()
+int led=2; // pin Led de verificacion
+void setup() 
 {
-  //Abrir el puerto de lectura en el PC para mensajes
-  Serial.begin(9600);
 
-  //----------- Comando para Conectarse a la WIFI el ESP8266 ---------
-  Serial.println("Conectandose a la WIFI!");
+  ///////////////////// rfid/////////////////
+   Serial.begin(9600);
+   SPI.begin(); // Init SPI bus
+   rfid.PCD_Init(); // Init MFRC522
+   Serial.println();
+   Serial.print(F("Reader :"));
+   rfid.PCD_DumpVersionToSerial();
+   for (byte i = 0; i < 6; i++) {
+     key.keyByte[i] = 0xFF;
+   } 
+   DatoHex = printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+   Serial.println();
+   Serial.println();
+   Serial.println("Iniciando el Programa");
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+   ///////////////////// vibrador/////////////////
+  pinMode(vb, INPUT);
+  pinMode(Avb, INPUT);
+  pinMode(led,OUTPUT);
+  
+}
+
+void loop() 
+{
+  ///////////////////// vibrador/////////////////
+  /*Dig_out = digitalRead(vb);
+  Ana_out = analogRead(Avb);
+  Serial.print("Anaolog : ");
+  int AnaV = Ana_out;//(Ana_out- 4095)*-1;
+  Serial.println(AnaV);
+  int AnaV1 = (Ana_out- 4095)*-1;
+  Serial.println(AnaV1);
+  Serial.print("          Digital :");
+  Serial.println(Dig_out);*/
+  
+  int vibracion = LeerVibracion(vb, Avb);
+  if (vibracion > 1000 and B==1){
+    digitalWrite(led,HIGH);
+    B = 0;
+    delay(500);
+  }else if (vibracion > 1000 and B==0){
+    digitalWrite(led,LOW);
+    B = 1;
     delay(500);
   }
-
-  Serial.println("");
-  Serial.println("WiFi conectada");
-  Serial.println(WiFi.localIP());
-  //----------- Fin de conección ESP8266 -----------------------------
-
-  //Establecer los modos de los puertos
-  //pinMode(sensorluzpin, INPUT);
-  pinMode(bombillopin, OUTPUT);
-  pinMode(ventiladorpin, OUTPUT);
-  pinMode(temperaturapin, INPUT);
-
-  //************ Conectar Cliente ThinkSpeak *******
-    ThingSpeak.begin( client );
-  //************ Fin Conectar Cliente ThingSpeak ***
   
-  //Inicializar el generador de numeros aleatorios
-  randomSeed(analogRead(0));
-}
+  delay(100);
 
-//metodo repetitivo
-unsigned long lastConnectionTime = 0;
-long lastUpdateTime = 0;
+  /////////////////// rfid /////////////////
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+     if ( ! rfid.PICC_IsNewCardPresent()){return;}
+     
+     // Verify if the NUID has been readed
+     if ( ! rfid.PICC_ReadCardSerial()){return;}
+     
+     Serial.print(F("PICC type: "));
+     MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+     Serial.println(rfid.PICC_GetTypeName(piccType));
+     // Check is the PICC of Classic MIFARE type
+     if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+     {
+       Serial.println("Su Tarjeta no es del tipo MIFARE Classic.");
+       return;
+     }
+     
+     if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3] )
+     {
+       Serial.println("Se ha detectado una nueva tarjeta.");
+       
+       // Store NUID into nuidPICC array
+       for (byte i = 0; i < 4; i++) {nuidPICC[i] = rfid.uid.uidByte[i];}
+    
+       DatoHex = printHex(rfid.uid.uidByte, rfid.uid.size);
+       Serial.print("Codigo Tarjeta: "); Serial.println(DatoHex);
 
-void loop()                    
-{
-  // Only update if posting time is exceeded
-  if (millis() - lastUpdateTime >=  postingInterval) {
-    lastUpdateTime = millis();
-    LeerSensores();
-    ImprimirValoresSensores();
-
-    //Verificar los umbrales
-    estadoventilador = UmbraldeTemperatura(umbralTemperatura);
-    estadobombillo = UmbraldeLuz(umbralLuz);
-
-    //Enviar los Datos a ThinkSpeak
-    write2TSData( channelID , dataFieldOne , temperatura , 
-                      dataFieldTwo , estadobombillo,
-                      dataFieldThree , estadoventilador,
-                      dataFieldFour, millis());     
-    }
+      int Resultado = verifivarRfid(UserReg, DatoHex);
+      Serial.println(Resultado);
+      ///
+       /*if(UserReg_1 == DatoHex)
+       {
+        Serial.println("USUARIO 1 - PUEDE INGRESAR");     
+       }
+       else if(UserReg_2 == DatoHex)
+       {
+        Serial.println("USUARIO 2 - PUEDE INGRESAR");
+       }
+       else if(UserReg_3 == DatoHex)
+       {
+        Serial.println("USUARIO 3 - PUEDE INGRESAR");
+       }
+       else
+       {
+        Serial.println("NO ESTA REGISTRADO - PROHIBIDO EL INGRESO");
+       }  
+       Serial.println();*/
+       ///
+     }
+     else 
+     {
+      Serial.println("Tarjeta leida previamente");
+      int Resultado = verifivarRfid(UserReg, DatoHex);
+      Serial.println(Resultado);
+     }
+     // Halt PICC
+     rfid.PICC_HaltA();
+     // Stop encryption on PCD
+     rfid.PCD_StopCrypto1();
+    delay(1000);
 }
